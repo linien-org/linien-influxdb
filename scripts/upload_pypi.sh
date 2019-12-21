6cca71bb7ca398d0f77dc7eaedf775439f010c55
Benjamin Wiegand <highwaychile@posteo.de>
2019-12-22 00:36:13 +0700

    fix scripts

--- scripts/upload_pypi.sh
+++ scripts/upload_pypi.sh
@@ -8,5 +8,5 @@ rm -R dist
 
 python3 setup.py sdist bdist_wheel
 echo 'enter pypi password'
-python3 -m twine upload --repository-url https://test.pypi.org/legacy/ dist/* -u hermitdemschoenenleben
-# python3 -m twine upload dist/* -u hermitdemschoenenleben
+# python3 -m twine upload --repository-url https://test.pypi.org/legacy/ dist/* -u hermitdemschoenenleben
+python3 -m twine upload dist/* -u hermitdemschoenenleben
